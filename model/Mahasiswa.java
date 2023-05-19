801afb604edfb8ccb1131c99169c2fbddb72bf3d
nnnaimrl <naimaaaibrahim@gmail.com>
2023-05-19 23:42:37 +0800

    Memanggil method dari banyak Class di package lain"

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+    public static void tampilanInfo(){
+        System.out.println();
+        System.out.println("== Data Mahasiswa =================");
+        System.out.println("NPM  : 2110010014");
+        System.out.println("Nama : Naima Nurul Hikmah");
+        System.out.println("===============================");
+    }
+}
